d71b89de4b9ac7dab7e8dbe9c3dd8559fb3062c4
Sukadha <sukadha>
2026-04-22 21:21:42 +0530

    latest

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,10 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import Techstack from "../About/Techstack";
-import Github from "../About/Github";
+// import Techstack from "../About/Techstack";
+// import Github from "../About/Github";
 import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
+  AiFillGithub
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
